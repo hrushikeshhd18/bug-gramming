bdbe5c434061285888e0ab37a1e235c68290f96f
HRUSHIKESH DESAI <48755786+hrushikeshhd18@users.noreply.github.com>
2020-07-29 10:43:57 +0530

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,14 @@
+const txtSet = [
+  "Front-end Developer",
+  "Back-end Developer",
+  "Android Developer",
+];
+
+// particle js config loader
+particlesJS.load(
+  "particle-class",
+  "assets/particlesjs-config.json",
+  function () {
+    console.log("callback - particles.js config loaded");
+  }
+);
